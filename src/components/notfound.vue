88746fdbbba12784653ac0abca51035b605dc915
Leinad <43974923+dabass2@users.noreply.github.com>
2019-09-28 23:42:32 -0500

    work on 404 page

--- src/components/notfound.vue
+++ src/components/notfound.vue
@@ -3,18 +3,7 @@
     <v-container fill-height grid-list-md text-xs-center>
       <v-layout align-center justify-center column>
         <v-card color="white" elevation="10" width="75vw">
-          <div class="text-xl-center display-4">
-            Files
-          </div>
-          <div class="text-xs-center title">
-            Overview of assorted files present on the site.
-          </div>
-          <div class="links flex-grow-1">
-            <v-breadcrumbs :items="vizu" divider=">"/>
-            <v-breadcrumbs :items="threeD" divider=">"/>
-            <v-breadcrumbs :items="dnd" divider=">"/>
-            <v-breadcrumbs :items="cgi" divider=">"/>
-          </div>
+          hehe
         </v-card>
       </v-layout>
     </v-container>
